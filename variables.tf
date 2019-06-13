d5498456c11ee0869c32df0b6947e4e5e2823569
Joost Saanen <joost@kabisa.nl>
2019-06-13 23:48:06 +0200

    Add SSM session manager

--- variables.tf
+++ variables.tf
@@ -170,3 +170,34 @@ variable "enable_rule_iam_password_policy" {
   description = "Specifies if 'IAM password policy' rule should be enabled"
   default     = false
 }
+
+### SSM Session Manager
+variable "enable_ssm_session_manager" {
+  description = "Specifies if the ssm session manager should be enabled"
+  default     = false
+}
+
+variable "s3_bucket_name" {
+  default     = ""
+  description = "(Optional) The name of bucket to store session logs. Specifying this enables writing session output to an Amazon S3 bucket."
+}
+
+variable "s3_key_prefix" {
+  default = ""
+  description = "(Optional) To write output to a sub-folder, enter a sub-folder name."
+}
+
+variable "s3_encryption_enabled" {
+  default = false
+  description = "(Optional) Encrypt log data."
+}
+
+variable "cloudwatch_log_group_name" {
+  default = ""
+  description = "(Optional) The name of the log group to upload session logs to. Specifying this enables sending session output to CloudWatch Logs."
+}
+
+variable "cloudwatch_encryption_enabled" {
+  default = false
+  description = "(Optional) Encrypt log data."
+}
