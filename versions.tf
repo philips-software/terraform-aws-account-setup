103255b7cabe8942f3181af17a48240bb318e471
Rik Tonnard <rik.tonnard@me.com>
2022-03-18 09:58:56 +0100

    Update required versions
    * The AWS provider should be at least 4, since resources are used for S3
      bucket server side encryption and policy.
    * The Terraform version should be at least 0.15 because the one function
      is used.

--- versions.tf
+++ versions.tf
@@ -1,4 +1,10 @@
-
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.15"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
